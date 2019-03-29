6662a8309b03e253a33aa0557e8b6ed3e2ccb9bf
mpavlov <mpavlov@infragistics.com>
2019-03-29 15:33:16 +0200

    Adding people view

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpint: 'http://localhost:15434/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiEndpint: 'http://localhost:15434/api'
 };
 
 /*
